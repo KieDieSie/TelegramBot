6e44aa6b2557c34482b8a384ca2a6568e31d8f87
KieDieSie <ku353048@gmail.com>
2024-11-13 21:45:46 +0200

    first tries

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello world')
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from decouple import config
+
+bot = Bot(token =config(TOKEN))
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.reply('Hello from my dev')
+
+@dp.message()
+async def echo(message: types.Message):
+    msg = message.text
+    if msg == 'abulday':
+        await message.answer('dabulday')
+    
+
+async def main():
+    await dp.start_polling(bot)
+    
+
+asyncio.run(main())
