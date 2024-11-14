354af4c36a68b305101d56cef34f0abfec636335
KieDieSie <ku353048@gmail.com>
2024-11-14 20:38:06 +0200

    i tried

--- main.py
+++ main.py
@@ -10,13 +10,42 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.reply('Hello from my dev')
+    while True:
+        await message.answer('Введіть перший ланцюг ДНК в такому вигляді: ААА-ААА-ААА-ААА-ААА... (якщо не відомо, введіть 0)')
+        firstDNK = message.text
+        await message.answer('Введіть другий ланцюг ДНК в такому вигляді: ААА-ААА-ААА-ААА-ААА... (якщо не відомо, введіть 0)')
+        secondDNK = message.text
+        await message.answer('Введіть ланцюг іРНК в такому вигляді: ААА-ААА-ААА-ААА-ААА... (якщо не відомо, введіть 0)')
+        rnk = message.text
+        await message.answer('Введіть масу гену (якщо не відомо, введіть 0)')
+        gen_mass = message.text
+        await message.answer('Введіть масу іРНК (якщо не відомо, введіть 0)')
+        RNK_mass = message.text
+        await message.answer('Введіть довжину гену(ДНК) (якщо не відомо, введіть 0)')
+        gen_len = message.text
+        await message.answer('Введіть масу білка (якщо не відомо, введіть 0)')
+        albumen_mass = message.text
+        await message.answer('Введіть кількість амінокислот (якщо не відомо, введіть 0)')
+        amino_amount = message.text
+        await message.answer('Введіть кількість інторонів(в кінці напишіть % якщо дано у відсодках, якщо дана маса інторонів - напишіть лише число) (якщо не відомо, введіть 0)')
+        intorns = message.text
+        
+        break
+    await message.answer(firstDNK)
+    await message.answer(secondDNK)
+    await message.answer(rnk)
+    await message.answer(gen_mass)
+    await message.answer(RNK_mass)
+    await message.answer(gen_len)
+    await message.answer(albumen_mass)
+    await message.answer(amino_amount)
+    await message.answer(intorns)
 
-@dp.message()
-async def echo(message: types.Message):
-    msg = message.text
-    if msg == 'abulday':
-        await message.answer('dabulday')
+# @dp.message()
+# async def echo(message: types.Message):
+#     msg = message.text
+#     if msg == 'abulday':
+#         await message.answer('dabulday')
     
 
 async def main():
